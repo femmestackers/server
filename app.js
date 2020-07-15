60408629cf2e7e77db4d4ed249fe566b8e4544fb
SumayaAlam19 <sumayaalam123@gmail.com>
2020-07-15 21:18:34 +1000

    server skeleton

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+//const mongoose = require("mongoose")
+
+const port = process.env.port || 3020
+
+const app = express()   
+
+app.use(cors())
+app.use(bodyParser.json())  
+
+app.get('/' , (req, res) => res.send("Welcome to SMOOTHIVERSE server"))
+
+app.listen(port, () => console.log("SMOOTHIVERSE server is running  on port "+ port))
