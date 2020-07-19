011f94a7e9fc23ea16b87fc64393d0fef7b908f0
SumayaAlam19 <sumayaalam123@gmail.com>
2020-07-19 18:55:08 +1000

    added auth passport
    Co-Authored-By: Ash <aishu1188@gmail.com>
    Co-Authored-By: Aubrey <aubree.gamboa@gmail.com>

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const User = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+});
+
+// plugin the passport-local-mongoose middleware with our User schema
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
