f584319f093429c1216edb218e160d62b90ffa61
SumayaAlam19 <sumayaalam123@gmail.com>
2020-07-29 20:38:12 +1000

    authenticated user function added in routes and controller

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const { register, login, logout } = require("../controllers/auth_controller")
+const { register, login, logout, authenticatedUser} = require("../controllers/auth_controller")
 
 router.post("/register", register)
 
-
+router.get("/user", authenticatedUser)
 router.post('/login', login)
 router.get('/logout', logout)
 
